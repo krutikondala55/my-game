56632f36a7cabbd217870e9a41671d8eee774f57
krutikondala55 <73785148+krutikondala55@users.noreply.github.com>
2020-12-30 18:19:21 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,12 +13,12 @@ var girls,girl1,girl2,girl3,girl4;
 var track, girl1img, girl2img, girl3img, girl4img;
 
 function preload(){
-  track = loadImage("images/track.jpg");
-  girl1img = loadImage("images/girl1.png");
-  girl2img = loadImage("images/girl2.png");
-  girl3img = loadImage("images/girl3.png");
-  girl4img = loadImage("images/girl4.png");
-  ground = loadImage("images/ground.png");
+  track = loadImage("track.jpg");
+  girl1img = loadImage("girl1.png");
+  girl2img = loadImage("girl2.png");
+  girl3img = loadImage("girl3.png");
+  girl4img = loadImage("girl4.png");
+  ground = loadImage("ground.png");
 }
 
 function setup(){
